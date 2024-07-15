6d3b29654b21519f9c1459ad5fb275d6915e3cd3
Md Parvej <mdparvejme24@gmail.com>
2024-07-15 16:43:46 +0600

    added semster router

---
+++ src/models/Semester.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const semesterSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Semester", semesterSchema);
