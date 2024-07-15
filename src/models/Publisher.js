030e63e3a6ee50b613db72a8cc5d9e1a6aca8cfa
Md Parvej <mdparvejme24@gmail.com>
2024-07-15 14:09:00 +0600

    added publisher model

---
+++ src/models/Publisher.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const publisherSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Publisher", publisherSchema);
